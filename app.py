e195073d681bac35b07d74155c0b43f989b8efbf
tcunnington <taylor.cunnington@gmail.com>
2018-06-11 13:14:59 -0700

    update runtime, gitignore

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+import os
 from flask import Flask, render_template, request, redirect
+from dotenv import load_dotenv
+
+load_dotenv()
+
+print(os.environ.get('PASSWORD'))
 
 app = Flask(__name__)
 
@@ -12,3 +18,9 @@ def about():
 
 if __name__ == '__main__':
   app.run(port=33507)
+
+
+# from flask.ext.sqlalchemy import SQLAlchemy
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[YOUR_DATABASE_NAME]'
+# db = SQLAlchemy(app)
